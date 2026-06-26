cc41154831f87766bd4b504b93aec2f950a2947d
Andrei Losev <losev_andrei@bk.ru>
2026-06-26 13:59:50 +0300

    fix datetime

--- lib/eapi/select.dart
+++ lib/eapi/select.dart
@@ -6,22 +6,43 @@ class Select {
   static const description = 'select rows from table';
 
   Future<Map<String, dynamic>?> call(Map<String, dynamic> params) async {
-    final db = DataBaseClient.getInstance();
-    final withCount = params['with_count'] == true;
-    final queryParams = Map<String, dynamic>.from(params)..remove('with_count');
+    try {
+      final db = DataBaseClient.getInstance();
+      final withCount = params['with_count'] == true;
+      final queryParams = Map<String, dynamic>.from(params)
+        ..remove('with_count');
 
-    final rows = await db.select(queryParams);
-    final result = <String, dynamic>{'rows': rows};
-    if (withCount) {
-      result['count'] = await db.count(queryParams);
+      final rows = await db.select(queryParams);
+      for (final (i, row) in rows.indexed) {
+        for (final key in row.keys) {
+          if (row[key] is DateTime) {
+            rows[i][key] = (row[key] as DateTime).toIso8601String();
+          }
+        }
+      }
+      final result = <String, dynamic>{'rows': rows};
+      if (withCount) {
+        result['count'] = await db.count(queryParams);
+      }
+
+      return result;
+    } catch (e, s) {
+      svc().logger.error([e, s]);
     }
-    return result;
   }
 
   static ServiceMethod createMethod() {
     return ServiceMethod(name, Select().call, description)
-      ..optional('where', 'list', '[column, operator, value] or list of conditions')
-      ..optional('orWhere', 'list', '[column, operator, value] or list of conditions')
+      ..optional(
+        'where',
+        'list',
+        '[column, operator, value] or list of conditions',
+      )
+      ..optional(
+        'orWhere',
+        'list',
+        '[column, operator, value] or list of conditions',
+      )
       ..optional('offset', 'u64', 'default: 0')
       ..optional('limit', 'u64')
       ..optional('order', 'list', '[column, asc|desc] or list of pairs')
